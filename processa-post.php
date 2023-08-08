bd4bfaf071a857a10ba169e97c4bf50dc85a85a4
Jhonatan Vinicius <JhonatanVSousa>
2023-08-08 10:33:49 -0300

    Exemplo GET finalizado

---
+++ processa-post.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Processamento POST</title>
+</head>
+<body>
+    <h1>Exemplo usando Post</h1>
+    <hr>
+
+    <p>Receber e processar dados via <b>POST</b></p>
+<?php
+
+?>
+
+<h2>Dados:</h2>
+<ul>
+    <li>Nome: </li>
+    <li>E-mail: </li>
+    <li>Mensagem: </li>
+</ul>
+    
+</body>
+</html>
